db45a320ed7a5c9b6b4dcd72f62a30996052a5e8
ilacin83655@gmail.com <ilacin83655@gmail.com>
2025-09-05 18:38:47 +0800

    gallery

---
+++ app/src/main/java/com/example/avtoapplication/Gallery.java
@@ -0,0 +1,20 @@
+package com.example.avtoapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Gallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+
+    }
+}
--- app/src/main/java/com/example/avtoapplication/MainActivity.java
+++ app/src/main/java/com/example/avtoapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.avtoapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +12,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class MainActivity extends AppCompatActivity {
+    
+    @SuppressLint("WrongViewCast")
+    private ImageButton fab;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fab = findViewById(R.id.Button_gallery);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,Gallery.class);
+                startActivity(intent);
+            }
+        });
+        
     }
 }
