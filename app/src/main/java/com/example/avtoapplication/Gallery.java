9b589c829f7a3ce2329afe0f7c289e7d02b433c5
ilacin83655@gmail.com <ilacin83655@gmail.com>
2025-09-08 18:19:31 +0800

    scrollHorizontIcon

--- app/src/main/java/com/example/avtoapplication/Gallery.java
+++ app/src/main/java/com/example/avtoapplication/Gallery.java
@@ -1,6 +1,10 @@
 package com.example.avtoapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,64 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Gallery extends AppCompatActivity {
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_gallery);
+
+    private HorizontalScrollView horizontalScrollView;
+
+    private ImageButton buttonScrollLeft;
+    private ImageButton buttonScrollRight;
+
+    public static final int SCROLL_DELTA = 100; // шаг нажатия на кнопку ScrollView
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
 
+        horizontalScrollView = findViewById(R.id.sroll);
+
+        buttonScrollLeft =  findViewById(R.id.leftButton);
+        buttonScrollRight =  findViewById(R.id.rightButton);
+
+       buttonScrollLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doScrollLeft();
+            }
+        });
+
+       buttonScrollRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doScrollRight();
+            }
+        });
+    }
+
+    private void doScrollLeft() {
+
+        int x = horizontalScrollView.getScrollX();
+        int y = horizontalScrollView.getScrollY();
+
+        if(x - SCROLL_DELTA >= 0) {
+            horizontalScrollView.scrollTo(x - SCROLL_DELTA, y);
+        }
 
     }
+
+    private void doScrollRight() {
+        int maxAmount = horizontalScrollView.getMaxScrollAmount();
+
+        int x = this.horizontalScrollView.getScrollX();
+        int y = this.horizontalScrollView.getScrollY();
+
+        if(x + SCROLL_DELTA <= maxAmount) {
+            this.horizontalScrollView.scrollTo(x + SCROLL_DELTA, y);
+        }
+    }
 }
+
+
