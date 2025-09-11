6c7d93cc24bcd77a169a73f25fdb793c6e3a4c7a
ilacin83655@gmail.com <ilacin83655@gmail.com>
2025-09-11 21:01:34 +0800

    miniature

--- app/src/main/java/com/example/avtoapplication/Bmw.java
+++ app/src/main/java/com/example/avtoapplication/Bmw.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,14 @@ public class Bmw extends AppCompatActivity {
     private ImageButton buttonScrollLeft;
     private ImageButton buttonScrollRight;
 
+    private ImageButton buttonBmwClickFoto;
+    private ImageButton buttonBmwClickFoto1;
+
+    boolean flag = true;
+
+//    private boolean isImageScaled = false;
+
+    private boolean isImageFitToScreen = false;
     public static final int SCROLL_DELTA = 100;
 
     @Override
@@ -32,6 +43,12 @@ public class Bmw extends AppCompatActivity {
         buttonScrollLeft =  findViewById(R.id.leftButton);
         buttonScrollRight =  findViewById(R.id.rightButton);
 
+
+        buttonBmwClickFoto = findViewById(R.id.imageBmw);
+        buttonBmwClickFoto1 = findViewById(R.id.imagebmw2);
+
+
+
         buttonScrollLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,6 +60,55 @@ public class Bmw extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 doScrollRight();
+            }
+        });
+
+//        // увелиение миниатуры фото прокрутки
+//            buttonBmwClickFoto.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if (!isImageScaled) v.animate().scaleX(5.4f).scaleY(5.4f).setDuration(500);
+//                    if (isImageScaled) v.animate().scaleX(1f).scaleY(1f).setDuration(500);
+//                    isImageScaled = !isImageScaled;
+
+        buttonBmwClickFoto.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if (isImageFitToScreen) {
+                isImageFitToScreen = false;
+
+                buttonBmwClickFoto.setLayoutParams(new LinearLayout.LayoutParams(200, 200));
+                buttonBmwClickFoto.setAdjustViewBounds(true);
+            } else {
+                isImageFitToScreen = true;
+                buttonBmwClickFoto.setLayoutParams(new LinearLayout.LayoutParams(600, 600));
+
+                buttonBmwClickFoto.setScaleType(ImageView.ScaleType.FIT_START);
+//                buttonBmwClickFoto.setImageResource(R.drawable.bmw_m5_vnut6);
+            }
+
+
+        }
+
+            });
+
+        buttonBmwClickFoto1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isImageFitToScreen) {
+                    isImageFitToScreen = false;
+
+                    buttonBmwClickFoto1.setLayoutParams(new LinearLayout.LayoutParams(200, 200));
+                    buttonBmwClickFoto1.setAdjustViewBounds(true);
+                } else {
+                    isImageFitToScreen = true;
+                    buttonBmwClickFoto1.setLayoutParams(new LinearLayout.LayoutParams(600, 600));
+
+                    buttonBmwClickFoto1.setScaleType(ImageView.ScaleType.FIT_START);
+//
+                }
             }
         });
     }
