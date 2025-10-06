3b84cdc63d68b85cf7281ef94268c08f58960cd1
ilacin83655@gmail.com <ilacin83655@gmail.com>
2025-10-06 16:07:47 +0800

    login_start_dis_buttom_login

--- app/src/main/java/com/example/avtoapplication/login/ActivityLoginStart.java
+++ app/src/main/java/com/example/avtoapplication/login/ActivityLoginStart.java
@@ -1,12 +1,12 @@
 package com.example.avtoapplication.login;
 
 
-
-
-
+import android.app.WallpaperManager;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -14,9 +14,16 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.load.resource.gif.GifDrawable;
 import com.example.avtoapplication.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ActivityLoginStart extends AppCompatActivity {
+
+    private int anycolor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,33 +31,107 @@ public class ActivityLoginStart extends AppCompatActivity {
         setContentView(R.layout.activity_login_start);
 
 
-        ImageButton loginButton = findViewById(R.id.loginBtn);
+
+
+//        ImageButton loginButton = findViewById(R.id.loginBtn);
 //        loginButton.setBackgroundColor(Color.TRANSPARENT);
 
-//        ImageButton loginButton = (ImageButton) this.findViewById(R.id.loginBtn);
+    //        ImageButton loginButton = findViewById(R.id.loginBtn);
 //        loginButton.setColorFilter(Color.argb(255, 255, 255, 255)); // White Tint
 
-        Button signButton = findViewById(R.id.sighUpBtn);
+//
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+
+
+//        ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
+//
+//        imageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(ActivityLoginStart.this, "ImageButton is clicked", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v) {
-                Toast.makeText(ActivityLoginStart.this, "ImageButton is clicked", Toast.LENGTH_SHORT).show();
+            public void onClick(View arg0) {
 
+                Toast.makeText(ActivityLoginStart.this,
+                        "Добро пожаловать",
+                        Toast.LENGTH_SHORT).show();
+                imageButton.requestFocus();
+//                Intent intent = new Intent(ActivityLoginStart.this, LoginActivity.class);
+//                startActivity(intent);
 
-        Intent intent = new Intent(ActivityLoginStart.this, LoginActivity.class);
-        startActivity(intent);
-
+                int timeout = 1000; //задержка на странице 1 сек
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        finish();
+                        Intent homepage = new Intent(ActivityLoginStart.this, LoginActivity.class);
+                        startActivity(homepage);
+                    }
+                }, timeout);
             }
+
         });
 
+
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//                boolean b = loginButton.isSelected() != loginButton.isSelected();
+//
+//                loginButton.setBackgroundResource(R.drawable.login3);
+//                Toast.makeText(ActivityLoginStart.this, "ImageButton is clicked", Toast.LENGTH_SHORT).show();
+//
+//
+//                Intent intent = new Intent(ActivityLoginStart.this, LoginActivity.class);
+//                startActivity(intent);
+
+//            }
+//        });
+//ImageButton signButton = findViewById(R.id.sighUpBtn);
+
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//                boolean b = loginButton.isSelected() != loginButton.isSelected();
+//
+//                loginButton.setBackgroundResource(R.drawable.login3);
+//                Toast.makeText(ActivityLoginStart.this, "ImageButton is clicked", Toast.LENGTH_SHORT).show();
+//
+//
+//                Intent intent = new Intent(ActivityLoginStart.this, LoginActivity.class);
+//                startActivity(intent);
+
+//            }
+//        });
+//
+        ImageButton signButton = findViewById(R.id.sighUpBtn);
         signButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ActivityLoginStart.this, SignUpActivity.class);
                 startActivity(intent);
             }
         });
+
+
     }
 }
